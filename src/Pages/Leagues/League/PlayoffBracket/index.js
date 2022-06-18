278d12a26753a6f361cdfeb9917bb83c70fc9753
anti-duhring <mateusvnlima@gmail.com>
2022-06-17 21:36:09 -0300

    Playoff bracket

--- src/Pages/Leagues/League/PlayoffBracket/index.js
+++ src/Pages/Leagues/League/PlayoffBracket/index.js
@@ -6,6 +6,7 @@ import ViewLightDark from "../../../../components/ViewLightDark";
 import { Entypo } from '@expo/vector-icons';
 import { DARK_BLACK, DARK_GRAY, DARK_GREEN, LIGHT_BLACK, LIGHT_GRAY, LIGHT_GREEN, WHITE } from "../../../../components/Variables";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import SkeletonPlaceholder from "react-native-skeleton-placeholder";
 
 const PlayoffBracket = ({route}) => {
     const leagueId = route.params?.leagueObject.league_id;
@@ -15,20 +16,26 @@ const PlayoffBracket = ({route}) => {
     const [playoffData, setPlayoffData] = useState(null);
     const [leagueRostersData, setLeagueRostersData] = useState(null);
     const [leagueUsersData, setLeagueUsersData] = useState(null)
-    const [round, setRound] = useState(1)
-    const playerNull = {
-        rosterID: 0,
-        userData: {
-            display_name: '{result} do matchup {matchup}',
-            avatar: null
-        },
-        rosterData: {
-            settings: {
-                wins: 0,
-                ties: 0,
-                losses: 0,
-                fpts: 0
-            }
+    const [round, setRound] = useState(1);
+    class PlayerNull {
+        constructor(name) {
+            this.rosterID = 0;
+            this.userData = {};
+            this.userData.display_name = name;
+            this.userData.avatar = null;
+            this.rosterData = {};
+            this.rosterData.settings = {};
+            this.rosterData.settings.wins = 0;
+            this.rosterData.settings.ties = 0;
+            this.rosterData.settings.losses = 0;
+            this.rosterData.settings.fpts = 0;
+        }
+    }
+    class Player {
+        constructor(rosterID) {
+            this.rosterID = rosterID;
+            this.rosterData = getPlayerRoster(rosterID);
+            this.userData = getPlayerInfo(this.rosterData.owner_id);
         }
     }
 
@@ -85,7 +92,7 @@ const PlayoffBracket = ({route}) => {
     }
 
     const RoundSelect = () => (
-        <ViewLightDark style={{flexDirection:'row',justifyContent:'center',alignContent:'center',alignItems:'center'}} containerStyle={{marginTop:20,width:150,height:40,}}>
+        <ViewLightDark style={styles.roundSelect} containerStyle={styles.roundSelectContainer}>
                 <TouchableOpacity 
                     disabled={(round==1) ? true : false}
                     onPress={() => setRound(round - 1)} 
@@ -96,8 +103,9 @@ const PlayoffBracket = ({route}) => {
                 
                 <Text style={{color:LIGHT_GRAY,flex:3,textAlign:'center'}}>Round {round}</Text>
                 <TouchableOpacity 
+                    disabled={(playoffData && playoffData[playoffData.length - 1].r == round) ? true : false}
                     onPress={() => setRound(round + 1)} 
-                    style={{flex:1}}
+                    style={{flex:1,opacity:(playoffData && playoffData[playoffData.length - 1].r == round) ? 0.2 : 1}}
                 >
                     <Entypo name="chevron-right" style={{textAlign:'center'}} size={24} color={LIGHT_GRAY} />
                 </TouchableOpacity>
@@ -105,36 +113,36 @@ const PlayoffBracket = ({route}) => {
         
     )
 
-    const PlayerMatchup = ({player, position, from}) => {
+    const PlayerAvatar = ({player}) => (
+        <Image source={(player.userData.avatar) ? {uri: `https://sleepercdn.com/avatars/${player.userData.avatar}`} : require('../../../../../assets/Images/player_default.png')} style={styles.playerAvatar} />
+    );
 
-        const Avatar = () => (
-            <Image source={(player.userData.avatar) ? {uri: `https://sleepercdn.com/avatars/${player.userData.avatar}`} : require('../../../../../assets/Images/player_default.png')} style={{width:50,height:50,borderRadius:50,resizeMode: 'cover',backgroundColor:DARK_BLACK}} />
-        );
-        const FantasyPoints = () => (
-            <Text style={[{fontSize:22,color:WHITE},(position=='left') ? {marginLeft:15} : {marginRight: 15}]}>
-                {player.rosterData.settings.fpts.toFixed(1)}
-            </Text>
-        );
+    const PlayerFantasyPoints = ({player, position}) => (
+        <Text style={[{fontSize:22,color:WHITE},(position=='left') ? {marginLeft:15} : {marginRight: 15}]}>
+            {player.rosterData.settings.fpts.toFixed(1)}
+        </Text>
+    );
 
+    const PlayerMatchup = ({player, position, winner}) => {
         return (
             <View style={{flex:1}}>
                 <View style={[styles.playerMatchupContainer,{justifyContent: (position=='left') ? 'flex-start' : 'flex-end'}]}>
                     {
                         (position=='left') ?
                         <>
-                            <Avatar />
-                            <FantasyPoints />
+                            <PlayerAvatar player={player} />
+                            <PlayerFantasyPoints player={player} position={position} />
                         </> :
                         <>
-                            <FantasyPoints />
-                            <Avatar />
+                            <PlayerFantasyPoints player={player} position={position} />
+                            <PlayerAvatar player={player} />
                         </>
                     }
 
                     </View>
                     <View style={{flex:1,alignItems:(position=='left') ? 'flex-start' : 'flex-end'}}>
                         <Text style={{color:WHITE, textAlign: (position=='left') ? 'left' : 'right'}}>    
-                            {(!from) ? player.userData.display_name : (from.w) ? player.userData.display_name.replace(/{matchup}/g,from.w).replace(/{result}/g,'Vencedor') : player.userData.display_name.replace(/{matchup}/g,from.l).replace(/{result}/g,'Perdedor')}
+                            { player.userData.display_name }
                         </Text>
                         <Text style={{color:DARK_GRAY}}>{player.rosterData.settings.wins}-{player.rosterData.settings.losses}{(player.rosterData.settings.ties!=0) ? '-'+player.rosterData.settings.ties : null}</Text>
                     </View>
@@ -145,12 +153,67 @@ const PlayoffBracket = ({route}) => {
     const Matchup = (props) => {
         return (
             <ViewLightDark style={{flexDirection:'row'}} title={`Matchup ${props.matchup.m}`} titleAlign='center'>
-                    <PlayerMatchup player={props.player1} position='left' from={props.matchup.t1_from} />
+                    <PlayerMatchup player={props.player1} position='left' winner={(props.matchup.w == props.player1.rosterID) ? true : false} />
                     <View style={styles.matchupVsContainer}>
                         <MaterialCommunityIcons name="sword-cross" size={35} color={LIGHT_GREEN} />
                     </View>
-                    <PlayerMatchup player={props.player2} position='right' from={props.matchup.t2_from} />
+                    <PlayerMatchup player={props.player2} position='right' winner={(props.matchup.w == props.player2.rosterID) ? true : false} />
             </ViewLightDark>
+        )
+    }
+
+    const PlayerPlaceholder = (props) => {
+        return (
+            <View style={{flex:1}}>
+                <View style={[styles.playerMatchupContainer,{justifyContent: (props.position=='left') ? 'flex-start' : 'flex-end'}]}>
+                    {
+                        (props.position=='left') ?
+                        <>
+                             <SkeletonPlaceholder highlightColor="#303840" backgroundColor="#262D33">
+                                <View style={styles.playerAvatar} />
+                            </SkeletonPlaceholder>
+                            <SkeletonPlaceholder highlightColor="#303840" backgroundColor="#262D33">
+                                <View style={{width:60, height:25,marginLeft:15,borderRadius: 4}} />
+                            </SkeletonPlaceholder>                       
+                        </> :
+                        <>
+                            <SkeletonPlaceholder highlightColor="#303840" backgroundColor="#262D33">
+                                <View style={{width:60, height:25,marginRight:15,borderRadius: 4}} />
+                            </SkeletonPlaceholder> 
+                            <SkeletonPlaceholder highlightColor="#303840" backgroundColor="#262D33">
+                                <View style={styles.playerAvatar} />
+                            </SkeletonPlaceholder>
+                        </>
+                    }
+
+                    </View>
+                    <View style={{flex:1,alignItems:(props.position=='left') ? 'flex-start' : 'flex-end'}}>
+                        <SkeletonPlaceholder highlightColor="#303840" backgroundColor="#262D33">
+                            <View style={{width:100, height:20,marginBottom:10,borderRadius: 4}} />
+                            <View style={{width:60, height:20,borderRadius: 4,alignSelf:(props.position=='left') ? 'flex-start' : 'flex-end'}} />
+                        </SkeletonPlaceholder>
+                    </View>
+            </View>
+        )
+    }
+
+    const MatchupPlaceholder = (props) => {
+        return (
+            <ViewLightDark>
+                <View style={{alignItems:'center'}}>
+                    <SkeletonPlaceholder highlightColor="#303840" backgroundColor="#262D33">
+                        <View style={{width:120, height:25,marginBottom:10,borderRadius: 4}} />
+                    </SkeletonPlaceholder>
+                </View>
+                <View style={{flexDirection:'row'}}>
+                <PlayerPlaceholder position='left' />
+                <View style={styles.matchupVsContainer}>
+                    <MaterialCommunityIcons name="sword-cross" size={35} color={LIGHT_GREEN} />
+                </View>
+                <PlayerPlaceholder position='right' />
+                </View>
+            </ViewLightDark>
+            
         )
     }
 
@@ -163,38 +226,46 @@ const PlayoffBracket = ({route}) => {
     return ( 
         <View style={{flex:1,backgroundColor:'#0B0D0F',}}>
             <HeaderLeagueContextProvider leagueObject={leagueObject}>
-                <TabTopLeague activeButton={route.params?.active} isAble={leagueDraftSettings ? true : false} leagueDraftSettings={leagueDraftSettings}   />
+                <TabTopLeague activeButton={route.params?.active} isAble={true} leagueDraftSettings={leagueDraftSettings}   />
 
                 <RoundSelect />
 
                 <View>
-                    {leagueRostersData && leagueUsersData && playoffData?.map((matchup, index) => {
+                    {(leagueRostersData && leagueUsersData && playoffData) ?
+                    
+                    
+                    playoffData.map((matchup, index) => {
                         if(matchup.r != round) return
 
                         let player1;
                         let player2;
 
                         if(matchup.t1) {
-                            player1 = {rosterID: matchup.t1}
-                            player1.rosterData = getPlayerRoster(player1.rosterID);
-                            player1.userData = getPlayerInfo(player1.rosterData.owner_id);
+                            player1 = new Player(matchup.t1);
+                        } else if(!matchup.t1 && matchup.t1_from) {
+                            player1 = new PlayerNull((matchup.t1_from.w) ? `Vencedor do matchup ${matchup.t1_from.w}` : `Perdedor do matchup ${matchup.t1_from.l}`)
+
                         } else {
-                            player1 = playerNull;
+                            player1 = new PlayerNull('Vaga em aberto');
                         }
 
                         if(matchup.t2) {
-                            player2 = {rosterID: matchup.t2}
-                            player2.rosterData = getPlayerRoster(player2.rosterID);
-                            player2.userData = getPlayerInfo(player2.rosterData.owner_id);
+                            player2 = new Player(matchup.t2);
+                        } else if(!matchup.t2 && matchup.t2_from) {
+                            player2 = new PlayerNull((matchup.t2_from.w) ? `Vencedor do matchup ${matchup.t2_from.w}` : `Perdedor do matchup ${matchup.t2_from.l}`)
                         } else {
-                            player2 = playerNull;
+                            player2 = new PlayerNull('Vaga em aberto');
                         }
                         
                             return (
                                 <Matchup matchup={matchup} player1={player1} player2={player2} key={index} />
                             )
                         
-                    })}
+                    }) : 
+                    <>
+                        <MatchupPlaceholder />
+                        <MatchupPlaceholder />
+                    </>}
                 </View>
             </HeaderLeagueContextProvider>
         </View>
@@ -217,5 +288,23 @@ const styles = StyleSheet.create({
     },
     matchupVsText: {
         color:LIGHT_GREEN,
+    },
+    playerAvatar: {
+        width:50,
+        height:50,
+        borderRadius:50,
+        resizeMode: 'cover',
+        backgroundColor:DARK_BLACK
+    },
+    roundSelect: {
+        flexDirection:'row',
+        justifyContent:'center',
+        alignContent:'center',
+        alignItems:'center'
+    },
+    roundSelectContainer: {
+        marginTop:20,
+        width:150,
+        height:40,
     }
 })
